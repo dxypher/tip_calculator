d7718f567cf73954f0203e9eebfdd7b7b6a37771
Nikhil <Home@c-24-5-28-219.hsd1.ca.comcast.net>
2013-09-18 16:32:23 -0700

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Nikhil on 9/18/13.
+//  Copyright (c) 2013 Nikhil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipcalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by Nikhil on 9/18/13.
+//  Copyright (c) 2013 Nikhil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
